167ff98bc9600533ea9e0f20842ee346464f7eb7
tuyenDev <tuyen@laptop>
2025-05-17 02:19:08 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(tokenTeleGramBot, { polling: true });
 const async = require("async");
 let chatId = 1941528902;
+const cron = require('node-cron');
+
 const app = express();
 const Link = require("./mp3.model.js");
 const play = require("./play.js");
@@ -14,9 +16,11 @@ app.use(express.json());
 
 
 
+cron.schedule('0 8 * * *', () => {
+  play(bot,chatId )
+});
+  play(bot,chatId )
 
-
-// play(bot,chatId )
 const queue = async.queue(async (msg, callback) => {
   if (msg.text.includes("https://www.youtube.com/")) {
     const newLink = new Link({ link: msg.text, numberRun: 0 });
--- play.js
+++ play.js
@@ -4,22 +4,30 @@ const infoVideo = require("./infoVideo.js");
 const Link = require("./mp3.model.js");
 const playMp3 = require("./playMp3.js");
 const path = require("path");
+
+const { DateTime } = require('luxon');
+
 async function play(bot, chatId) {
   await bot.sendMessage(chatId, `Bắt đầu chạy chương trình`);
 
   let pathChaoCo = path.join(__dirname, "thuongco.mp3");
-  await playMp3(pathChaoCo);
+  await playMp3(pathChaoCo, 200);
 
   for (let i = 0; i < 20; i++) {
+    const now = DateTime.now().setZone('Asia/Bangkok');
+    if (now.hour >= 21) {
+        break;
+    }
     await bot.sendMessage(chatId, `Chạy lần ${i + 1}`);
     const mp3 = await Link.findOneAndUpdate(
       {
         numberRun: 0,
       },
-      { $inc: { numberRun: 0 } }
+      { $inc: { numberRun: 1 } }
     ).lean();
     if (!mp3) {
       await bot.sendMessage(chatId, "Đã hết link phát");
+      process.exit(1)
     }
     const linkYTB = mp3.link;
     const idVideo = linkYTB.split("/")[3].slice(8);
@@ -28,10 +36,17 @@ async function play(bot, chatId) {
     const resDowloadMp3 = await downloadYouTubeMP3(idVideo);
     const ifVideo = await infoVideo(idVideo);
     const titleVideo = ifVideo.title;
+    const duraVideo = ifVideo.duration
     await bot.sendMessage(chatId, `Bắt đầu chạy: ${titleVideo}`);
     const pathMp3 = path.join(__dirname, resDowloadMp3);
-    const playVoice = await playMp3(pathMp3);
+    const playVoice = await playMp3(pathMp3,duraVideo);
     await deleteMp3(pathMp3);
   }
+  let pathThongbao = path.join(__dirname, 'thongbaodingu.mp3')
+  await playMp3(pathThongbao, 10 )
+  let pathBai1 = path.join(__dirname, 'bac_1.mp3')
+    await playMp3(pathBai1, 200 )
+    
+
 }
 module.exports = play;
